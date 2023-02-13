67c9c6779d47a1092358857284016c3e2d543889
祝奇 <zhuqi@lswooks.com>
2023-02-13 11:45:48 +0800

    feat: 修复当使用据对路径时，线上报错

--- script/genReadme.js
+++ script/genReadme.js
@@ -5,7 +5,7 @@ import fs from "fs";
 // const __filenameNew = fileURLToPath(import.meta.url)
 // const __dirnameNew = path.dirname(__filenameNew)
 
-const wrapperRoot = './docs/'
+const wrapperRoot = './../docs/'
 const pathList = ['base', 'advance', 'interview', 'other', 'temp']
 
 pathList.forEach(item => {
