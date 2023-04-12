261f692cfd4bae9672f648d9f9a22499df93523f
祝奇 <zhuqi@lswooks.co>
2023-04-12 11:10:43 +0800

    feat: todo

---
+++ docs/base/JavaScript/从0到1开发一个npm包.md
@@ -0,0 +1,35 @@
+# 从0到1开发一个npm包
+
+## 文档
+
+## 构建
+
+## 规范
+
+## 测试
+
+## 可持续集成
+
+## 其他内容
+
+## 总结
+
+[如何写一个现代的JavaScript库](https://juejin.cn/post/6844903687819640845#heading-6)
+
+
+
+
+
+```
+export default {
+  vue官网: 'https://cn.vuejs.org/v2/api/',
+  vueAntPro文档: 'https://www.antdv.com/components/icon-cn/',
+  用户认证中心: 'http://ssocenter.lishicloud.com/#/login',
+  素材中心:
+    'http://material.lishicloud.com/index.html#/login?url=%2FgeneralMaterial%2FiconManage%2Fgeneral',
+  echart官网: 'https://echarts.apache.org/zh/option.html#title',
+  lodash文档: 'https://lodash.com/docs/4.17.15',
+  高德地图文档: 'https://lbs.amap.com/api/javascript-api/summary',
+  正则大全: 'https://any86.github.io/any-rule/'
+}
+```
---
+++ docs/other/work/todo/2023/todo.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+
+let start = 5, year = 2023
+
+for (; start <= 12; start++) {
+    // 创建文件
+    let content = ''
+    // 追加内容
+    const startData = new Date(year, start, 0)
+    const date = startData.getDate()
+    for (let j = 1; j <= date ; j++) {
+        const _curDate = new Date(year, start - 1, j)
+        const week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+        console.log(`${year}年${start}月${j}日 ${week[_curDate.getDay()]}`)
+        content += `
+## ${year}年${start}月${j}日 ${week[_curDate.getDay()]}
+
+- [ ] 日报、tabd、打卡
+- [ ] 跑步30分钟
+        `
+    }
+
+    fs.writeFile(`./${start}月.md`, content, (err, data) => {
+        if (!err) {
+            console.log('创建成功~')
+        }
+    })
+}
