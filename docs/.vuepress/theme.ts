1a4153372aff349ceb481b4b2b799fabbf0c22c2
zhuqi <zhuqi>
2024-03-06 17:01:09 +0800

    feat: 部署到华为云

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -54,6 +54,7 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
+    git: false,
     // 你应该自行生成自己的评论服务
     // comment: {
     //   provider: "Giscus",
@@ -68,6 +69,15 @@ export default hopeTheme({
     components: {
       components: ["Badge", "VPCard"],
     },
+
+    copyright: {
+      global: true,
+      disableCopy: true,
+      disableSelection: true
+    },
+
+    copyCode: params.mode === 'dev',
+
 
     // 此处开启了很多功能用于演示，你应仅保留用到的功能。
     mdEnhance: {
